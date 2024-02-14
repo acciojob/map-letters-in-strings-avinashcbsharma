18cb6e76b7d9916d37006ea970972238eb6fb951
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-14 15:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function createIndexObject(word) {
+  const indexObject = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = Symbol.for(word[i]);
+
+    if (!indexObject[letter]) {
+      indexObject[letter] = [];
+    }
+
+    indexObject[letter].push(i);
+  }
+
+  return indexObject;
+}
